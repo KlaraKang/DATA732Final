a075dcbdb49d549afc80861700c902d1171d03a6
KyoungA Kang <kkang@gradcenter.cuny.edu>
2022-04-29 13:57:47 -0400

    removed iframe

---
+++ main.js
@@ -0,0 +1,11 @@
+import {map} from './map.js'
+import {states} from './states.js'
+import {biasVict} from './biasVict.js'
+import {offense} from './offense.js'
+import {location} from './location.js'
+
+map();
+states();
+biasVict();
+offense();
+location;
